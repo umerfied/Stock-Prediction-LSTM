917f0784f9b42b84a8a0b50f236449d92ff3afd6
Umer <momersameer5@gmail.com>
2025-01-24 00:42:02 +0500

    Changed the directory of the files

---
+++ Stock Prediction using LSTM/config.py
@@ -0,0 +1,7 @@
+# Configuration parameters
+TICKER = 'AAPL'
+TIME_PERIOD = '5y'  # (available 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max)
+TIME_INTERVAL = '1d'
+WINDOW_SIZE = 3
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.9 
